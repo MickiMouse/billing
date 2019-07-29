7e1f95d7a11ef152f7020f921b191c10f49271c2
MickeyMouse <495lolkek42@gmail.com>
2019-07-29 18:41:57 +0300

    first commit BE

---
+++ backend/main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# class Reseller(AbstractUser):
+#     pass
+#
+#
+# class TvAdmin(AbstractUser):
+#     pass
+#
+#
+# class Card(models.Model):
+#     pass
+#
+#
+# class Subscriber(models.Model):
+#     pass
+#
