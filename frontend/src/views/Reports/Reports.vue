df460fd80cf5d75652a7d37d456eef436310ee1b
apple <gayvoronsky.m@ideasoft.io>
2019-07-29 19:50:25 +0300

    fixed

--- frontend/src/views/Reports/Reports.vue
+++ frontend/src/views/Reports/Reports.vue
@@ -31,6 +31,7 @@
                 crossDomain: true
 
             };
+            axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
             axios
                 .get('http://localhost:8000/api/test/',config)
                 .then(response => (this.info = response));
